fcef7b570854c2a99a02183c303570967c74c149
Catalin Rotariu <catalin.rotariu@globallogic.com>
2025-06-12 14:19:03 +0300

    Add docker file for billing service

---
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.pm.billing_service.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("creatingBillingAccount request received {}", request.toString());
+
+        //Business logic - save to db etc
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+}
