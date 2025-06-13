4cab39361432bf856958337d2fa2a1032959f8ff
Catalin Rotariu <catalin.rotariu@globallogic.com>
2025-06-13 15:57:34 +0300

    Add jwt token to auth service

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+        String token = tokenOptional.get();
+
+        return  ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
---
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,3 @@
+package com.pm.authservice.dto;
+
+public record LoginResponseDTO(String token) {}
